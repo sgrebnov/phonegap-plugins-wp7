6df0cdf15a5817eadcdf44d4c34711bf45a6569d
Sergei Grebnov <sergey.grebnov@akvelon.com>
2012-01-27 14:08:31 +0400

    Implemented plugin to get current phone theme info

---
+++ tests/MobileSpecUnitTests/PhoneTheme.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.Serialization;
+using WP7GapClassLib.PhoneGap;
+using WP7GapClassLib.PhoneGap.Commands;
+using WP7GapClassLib.PhoneGap.JSON;
+using Microsoft.Phone.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Phone.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PhoneGap.Extension.Commands
+{
+
+    public class PhoneTheme : BaseCommand
+    {
+        #region Constants
+
+        #endregion
+
+        #region Live tiles options
+
+        /// <summary>
+        /// Represents selected on device theme
+        /// </summary>
+        [DataContract]
+        public class PhoneThemeInfo
+        {
+ 
+            [DataMember(IsRequired = false, Name = "backgroundColor")]
+            public string BackgroundColor { get; set; }
+
+            [DataMember(IsRequired = false, Name = "isDark")]
+            public bool IsDark { get; set; }
+
+            [DataMember(IsRequired = false, Name = "isLight")]
+            public bool IsLight { get; set; }
+
+            [DataMember(IsRequired = false, Name = "accentColor")]
+            public string AccentColor { get; set; }
+
+        }
+        #endregion
+
+        public void get(string options)
+        {
+                      
+            try
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    try
+                    {
+                        PhoneThemeInfo themeInfo = new PhoneThemeInfo();
+
+                        themeInfo.BackgroundColor = ((Color)Application.Current.Resources["PhoneBackgroundColor"]).ToString();
+
+                        themeInfo.IsLight = (Visibility)Application.Current.Resources["PhoneLightThemeVisibility"] == Visibility.Visible;
+                        themeInfo.IsDark = (Visibility)Application.Current.Resources["PhoneDarkThemeVisibility"] == Visibility.Visible;
+
+                        themeInfo.AccentColor = ((Color)Application.Current.Resources["PhoneAccentColor"]).ToString();
+
+                        DispatchCommandResult(new PluginResult(PluginResult.Status.OK, themeInfo));
+                    }
+                    catch (Exception e)
+                    {
+                        DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, e.Message));
+                    }
+
+                });                                
+            }
+            catch (Exception e)
+            {
+                DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, "Error updating application tile"));
+            }
+        }       
+
+    }
+}
